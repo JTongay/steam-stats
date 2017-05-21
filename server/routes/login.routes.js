3bd3735fc49a401480243873d7e0e23ac5a5596c
Joseph Tongay <dirk.tongay@gmail.com>
2017-05-21 14:43:29 -0500

    password hashing and verification redone in bcrypt. i thought it wouldnt work with node 7 but it does

--- server/routes/login.routes.js
+++ server/routes/login.routes.js
@@ -7,7 +7,7 @@ const router = express.Router({
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../models/User.model')
-const scrypt = require('scrypt')
+const bcrypt = require('bcrypt')
 
 router.post('/signup', (req, res, next)=>{
   let reqUsername = req.body.user
@@ -23,12 +23,11 @@ router.post('/signup', (req, res, next)=>{
       res.json({status: "You fucked up"})
       return
     }
-    scrypt.kdf(reqPassword, {N: 1, r:1, p:1}, (err, result)=>{
-      console.log(result.toString("base64"));
-      User.create({username: reqUsername, password: result, steamID: null}).then((usr)=>{
+    bcrypt.hash(reqPassword, 12, (err, hash)=>{
+      User.create({username: reqUsername, password: hash, steamID: null}).then((usr)=>{
         res.json(usr)
       })
-    });
+    })
   })
 
 })
@@ -36,33 +35,15 @@ router.post('/signup', (req, res, next)=>{
 router.post('/login', (req, res, next)=>{
   let reqUsername = req.body.user
   let reqPassword = req.body.pass
-  let scryptParams = scrypt.paramsSync(0.1)
-  let kdfResult = scrypt.kdfSync(reqPassword, scryptParams)
-  console.log("scryptParams", scryptParams);
-  console.log("kdfResult", kdfResult);
-  User.findOne({username: reqUsername}).then((err, user)=>{
-
-    // if(err){
-    //   console.log(err, "error")
-    //   res.json(err)
-    //   return
-    // }
-
-    if(user){
-      console.log("user fuck up", user);
-      res.json({status: "You fucked up"})
-      return
+  User.findOne({username: reqUsername}).then((user, err)=>{
+    if(err){
+      console.log(err, "error")
     }
 
-    console.log(user)
-
-    scrypt.verifyKdf(kdfResult, reqPassword).then((res)=>{
-        console.log("Passwords Match", res);
-
-    },(err)=>{
-        console.log("Passwords Don't Match", err);
+    bcrypt.compare(reqPassword, user.password, (err, pass)=>{
+      console.log(pass, "password verification")
     })
-
+    
   })
 
 })
