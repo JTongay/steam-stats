04a8afb6a80f8c1041c56670d86a3a5172d4c2ab
Joseph Tongay <dirk.tongay@gmail.com>
2017-05-23 23:36:49 -0500

    i think ill go back to the db strategy to reduce chatter

--- src/app/routing.js
+++ src/app/routing.js
@@ -26,36 +26,23 @@ export default function routing($stateProvider, $urlRouterProvider, $locationPro
     url: '/',
     template: require('./components/Home/Home.html'),
     controller: HomeComponent,
-    controllerAs: 'home',
-    resolve: {
-      steamUser: ['$http', '$state', function($http, $state){
-        return $http.get("/auth/steam/checkuser")
-            .then((res)=>{
-              console.log(res, "in the front end");
-              // $state.go('home')
-              return res
-            })
-            .catch((err)=>{
-              console.log(err, "error in the front end");
-              return err
-            })
-          }]
-    }
+    controllerAs: 'home'
+    // resolve: {
+    //   steamUser: ['$http', '$state', function($http, $state){
+    //     return $http.get("/auth/steam/checkuser")
+    //         .then((res)=>{
+    //           console.log(res, "in the front end");
+    //           // $state.go('home')
+    //           return res
+    //         })
+    //         .catch((err)=>{
+    //           console.log(err, "error in the front end");
+    //           return err
+    //         })
+    //       }]
+    // }
   })
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
-  // $httpProvider.interceptors.push(function($location) {
-  //     return {
-  //       response: function(response) {
-  //         // do something on success
-  //         console.log(response, "front end");
-  //         return response;
-  //       },
-  //       responseError: function(response) {
-  //         if (response.status === 401)
-  //           $location.url('/');
-  //         return response;
-  //       }
-  //     };
-  //   });
+  
 }
