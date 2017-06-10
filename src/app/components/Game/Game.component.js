b471ee7873968a4bda265b21142555a24cea7618
Joseph Tongay <dirk.tongay@gmail.com>
2017-06-09 19:50:39 -0500

    game data is on a different state

--- src/app/components/Game/Game.component.js
+++ src/app/components/Game/Game.component.js
@@ -6,6 +6,18 @@ export default class GameComponent {
     this._steamSearchService = steamSearchService
     this.playedGames = this._user.grabPlayedGamesData()
     console.log(this.playedGames)
+    this.ownedGamesInfo = []
+    //Get game information for each game owned.
+    this.playedGames.forEach((game)=>{
+      this._steamSearchService.getGameInfo(game.appid).then((res, err)=>{
+        //sometimes games dont exist with a certain ID. Filter out bad requests
+        if(res.data[game.appid].success){
+          this.ownedGamesInfo.push(res.data[game.appid])
+        }
+      })
+    })
+    console.log(this.ownedGamesInfo, "ownedGamesInfo");
+
   }
 
 }
