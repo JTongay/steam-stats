bd70d170c13e21faa93459fc6da490891c7cfd58
Joseph Tongay <dirk.tongay@gmail.com>
2017-06-19 18:43:14 -0500

    im such a doofus

--- src/app/components/Game/Game.component.js
+++ src/app/components/Game/Game.component.js
@@ -5,19 +5,22 @@ export default class GameComponent {
     this._user = user
     this._steamSearchService = steamSearchService
     this.playedGames = this._user.grabPlayedGamesData()
-    console.log(this.playedGames)
     this.ownedGamesInfo = []
     //Get game information for each game owned.
     this.playedGames.forEach((game)=>{
       this._steamSearchService.getGameInfo(game.appid).then((res, err)=>{
         //sometimes games dont exist with a certain ID. Filter out bad requests
         if(res.data[game.appid].success){
-          this.ownedGamesInfo.push(res.data[game.appid])
+          this.ownedGamesInfo.push(res.data[game.appid].data)
         }
       })
     })
     console.log(this.ownedGamesInfo, "ownedGamesInfo");
 
+  }
+
+  selectThisGame(game){
+    return this._user.stashGameData(game)
   }
 
 }
