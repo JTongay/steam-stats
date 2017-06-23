007b5439afeef7a3ca73b28f16823f0fdea8c432
Joseph Tongay <dirk.tongay@gmail.com>
2017-06-22 21:15:11 -0500

    omg im such an idiot. im not using lifecycle hooks at all. lets do that

--- src/app/components/GameInfo/GameInfo.component.js
+++ src/app/components/GameInfo/GameInfo.component.js
@@ -5,17 +5,22 @@ export default class GameInfoComponent {
     this._steamSearchService = steamSearchService
     this._user = user
     this._$localStorage = $localStorage
-    this.usersSteamID = $localStorage.steamID
-    this.game = user.grabGameData()
+  }
+
+  $onInit(){
+    this.game = this._user.grabGameData()
     console.log(this.game)
-    this.globalAchievement = steamSearchService.getGlobalAchievementPercentForGame(this.game.steam_appid)
-    console.log(this.globalAchievement, 'globalAchievement');
-    this.playerAchievement = steamSearchService.getPlayerAchievementsForGame(this.usersSteamID, this.game.steam_appid)
-    console.log(this.playerAchievement, 'playerAchievement');
-    steamSearchService.getGameSchema(this.game.steam_appid).then((res, err)=>{
-      this.gameSchema = res.data.game.availableGameStats.achievements
-      console.log(this.gameSchema, "gameSchema");
-    })
+    // this.usersSteamID = $localStorage.steamID
+    // this.globalAchievement = steamSearchService.getGlobalAchievementPercentForGame(this.game.steam_appid)
+    // console.log(this.globalAchievement, 'globalAchievement');
+    // steamSearchService.getPlayerAchievementsForGame(this.usersSteamID, this.game.steam_appid).then((res, err)=>{
+    //   this.playerAchievement = res.data.playerstats.achievements
+    //   console.log(this.playerAchievement, 'playerAchievement');
+    // })
+    // steamSearchService.getGameSchema(this.game.steam_appid).then((res, err)=>{
+    //   this.gameSchema = res.data.game.availableGameStats.achievements
+    //   console.log(this.gameSchema, "gameSchema");
+    // })
   }
 
 }
