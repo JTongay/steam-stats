b002a5d61efba7a97da5551c95fe0893741a5054
Joseph Tongay <dirk.tongay@gmail.com>
2017-06-26 22:09:59 -0500

    man this api really sucks

--- src/app/components/GameInfo/GameInfo.component.js
+++ src/app/components/GameInfo/GameInfo.component.js
@@ -11,16 +11,26 @@ export default class GameInfoComponent {
     this.game = this._user.grabGameData()
     console.log(this.game)
     this.usersSteamID = this._$localStorage.steamID
-    this.globalAchievement = this._steamSearchService.getGlobalAchievementPercentForGame(this.game.steam_appid)
-    console.log(this.globalAchievement, 'globalAchievement');
-    this._steamSearchService.getPlayerAchievementsForGame(this.usersSteamID, this.game.steam_appid).then((res, err)=>{
-      this.playerAchievement = res.data.playerstats.achievements
-      console.log(this.playerAchievement, 'playerAchievement');
+
+    this._steamSearchService.getPlayerAchievementsForGame(
+      this.usersSteamID,
+      this.game.steam_appid
+    ).then((res, err)=>{
+        this.playerAchievement = res.data.playerstats.achievements
+        this._steamSearchService.getGlobalAchievementPercentForGame(this.game.steam_appid).then((res, err)=>{
+          this.globalAchievement = res.data.achievementpercentages.achievements
+          this.playerAchievement.map((achievement, index)=>{
+            achievement.globalPercent = this.globalPercent[index]
+          })
+          console.log(this.playerAchievement, "playerAchievement");
+      })
     })
+
+    console.log(this.playerAchievement, 'playerAchievement');
     this._steamSearchService.getGameSchema(this.game.steam_appid).then((res, err)=>{
       this.gameSchema = res.data.game.availableGameStats.achievements
-      console.log(this.gameSchema, "gameSchema");
+      return this.gameSchema
     })
+    console.log(this.gameSchema, "gameSchema");
   }
-
 }
