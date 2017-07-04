897a5ed55b12729a2adf0174988d897cdc919cbf
Joseph Tongay <dirk.tongay@gmail.com>
2017-07-03 19:49:07 -0500

    using route params instead of a selected game in a service

--- src/app/components/GameInfo/GameInfo.component.js
+++ src/app/components/GameInfo/GameInfo.component.js
@@ -12,15 +12,14 @@ export default class GameInfoComponent {
     this._steamSearchService.getGameInfo(this._$stateParams.selectedGame).then((res, err)=>{
       this.game = res.data[this._$stateParams.selectedGame].data
     })
-    console.log(this)
     this.usersSteamID = this._$localStorage.steamID
 
     this._steamSearchService.getPlayerAchievementsForGame(
       this.usersSteamID,
-      this.game.steam_appid
+      this._$stateParams.selectedGame
     ).then((res, err)=>{
         let playerAchievement = res.data.playerstats.achievements
-        this._steamSearchService.getGlobalAchievementPercentForGame(this.game.steam_appid).then((res, err)=>{
+        this._steamSearchService.getGlobalAchievementPercentForGame(this._$stateParams.selectedGame).then((res, err)=>{
           let globalAchievement = res.data.achievementpercentages.achievements
           playerAchievement.forEach((achievement, pIndex)=>{
             globalAchievement.forEach((globalAchievement, gIndex)=>{
@@ -33,7 +32,7 @@ export default class GameInfoComponent {
           console.log(this.playerAchievement, "playerAchievement");
       })
     })
-    this._steamSearchService.getGameSchema(this.game.steam_appid).then((res, err)=>{
+    this._steamSearchService.getGameSchema(this._$stateParams.selectedGame).then((res, err)=>{
       this.gameSchema = res.data.game.availableGameStats.achievements
       console.log(this.gameSchema);
     })
